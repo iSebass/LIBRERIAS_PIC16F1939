414f01ad8c0f04928dee5d162e170bf31c69700e
iSebas <97457868+iSebass@users.noreply.github.com>
2024-03-16 09:32:40 -0500

    Update LCD_LIB.h

--- LIBRERIAS/LCD_LIB/LCD_LIB.h
+++ LIBRERIAS/LCD_LIB/LCD_LIB.h
@@ -1,63 +1,162 @@
-/* 
- * File:   LCD_LIB.h
- * Author: iProf
- *
- * Created on 1 de septiembre de 2023, 08:06 AM
- */
-
 #ifndef LCD_LIB_H
 #define	LCD_LIB_H
 
 #include <xc.h>
-#include "config.h"
-
-#define _LCD_CLEAR             1
-#define _LCD_RETURN_HOME       2
-#define _LCD_BLINK_CURSOR_ON   15
-#define _LCD_CURSOR_OFF        12
-#define _LCD_UNDERLINE_ON      14
-#define _LCD_FIRST_ROW         128
-#define _LCD_SECOND_ROW        192
-#define _LCD_THIRD_ROW         148
-#define _LCD_FOURTH_ROW        212
-#define _LCD_MOVE_CURSOR_LEFT  16
-#define _LCD_MOVE_CURSOR_RIGHT 20
-#define _LCD_TURN_OFF          8
-#define _LCD_TURN_ON           15
-#define _LCD_SHIFT_LEFT        24
-#define _LCD_SHIFT_RIGHT       28
-
-#define DIR_RS    TRISDbits.TRISD0
-#define DIR_EN    TRISDbits.TRISD1
-#define DIR_D4    TRISDbits.TRISD4
-#define DIR_D5    TRISDbits.TRISD5
-#define DIR_D6    TRISDbits.TRISD6
-#define DIR_D7    TRISDbits.TRISD7
-
-#define LAT_RS    LATDbits.LATD0
-#define LAT_EN    LATDbits.LATD1
-#define LAT_D4    LATDbits.LATD4
-#define LAT_D5    LATDbits.LATD5
-#define LAT_D6    LATDbits.LATD6
-#define LAT_D7    LATDbits.LATD7
-
-#define ANSEL_RS  ANSELDbits.ANSD0
-#define ANSEL_EN  ANSELDbits.ANSD1
-#define ANSEL_D4  ANSELDbits.ANSD4
-#define ANSEL_D5  ANSELDbits.ANSD5
-#define ANSEL_D6  ANSELDbits.ANSD6
-#define ANSEL_D7  ANSELDbits.ANSD7
-
-// Cremos el prototipo de funcion
-void LCD_Init(void);
-void LCD_Cmd(char cmd);
-void LCD_Write_Cp(char letra);
-void LCD_Write_Text_Cp(char *text);
-void LCD_Set_Cursor(char fila, char col);
-void LCD_Write(char fila, char col, char letra);
-void LCD_Write_Text(char fil, char col, char *text);
+#define _delay_ms(x)   __delay_ms(x)
+#define _delay_us(x)   __delay_us(x)
 
 
+/************************************************************************/
+/* Comportamiento de RS                                                 */
+/************************************************************************/
+#define _COMMAND_      0
+#define _DATA_         1
 
-#endif	/* LCD_LIB_H */
+/************************************************************************/
+/* DEFINIR EL PUERTO DONDE SE VA CONECTAR LA LCD                        */
+/************************************************************************/
+#define _LCD_ANSEL  ANSELD
+#define _LCD_DDR    TRISD
+#define _LCD_PORT   PORTD
+
+#define _LCD_RS    0
+#define _LCD_EN    1
+#define _LCD_D4    4
+#define _LCD_D5    5
+#define _LCD_D6    6
+#define _LCD_D7    7
+
+
+#define _LCD_nCOL_       20
+#define _LCD_nROW_       4
+
+/************************************************************************/
+/*  MODO CELAR DISPLAY:  D7 D6 D5 D4 D3 D2 D1 D0                        */
+/*                       0  0  0  0  0  0  0  1                         */
+/************************************************************************/
+#define _LCD_CLEARDISPLAY 0x01
+
+/************************************************************************/
+/*  MODO CELAR RETURN HOME:  D7 D6 D5 D4 D3 D2 D1 D0                    */
+/*                           0  0  0  0  0  0  1  0                     */
+/************************************************************************/
+#define _LCD_RETURNHOME     0x02
+
+/************************************************************************/
+/*      ENTRAMOS EN MODE SET:  D7 D6 D5 D4 D3 D2 D1  D0                 */
+/*      					   0  0  0  0  0  1  I/D  S                 */
+/*----------------------------------------------------------------------*/
+/*      I/D = 1: Inc                                                    */
+/*		      0: Dec                                                    */
+/*		S   = 1: SHIFT ON                                               */
+/*            0: SHIFT OFF                                              */
+/************************************************************************/
+#define _LCD_ENTRYMODESET   0x04
+#define _LCD_INCREMENT      0x02
+#define _LCD_DECREMENT      0x00
+#define _LCD_SHIFT_ON       0x01
+#define _LCD_SHIFT_OFF      0x00
+
+/************************************************************************/
+/*      ENTRAMOS EN DISPLAY CONTROL:  D7 D6 D5 D4  D3 D2 D1 D0          */
+/*      						      0  0  0  0   1  D  U  B           */
+/*----------------------------------------------------------------------*/
+/*      D   = 1: DISPLAY ON                                             */
+/*		      0: DISPLAY OFF                                            */
+/*		U   = 1: CURSOR ON                                              */
+/*		      0: CURSOR OFF                                             */
+/*		B   = 1: BLINK                                                  */
+/*		      0: NO BLINK                                               */
+/************************************************************************/
+#define _LCD_DISPLAYCONTROL 0x08
+#define _LCD_DISPLAY_ON     0x04
+#define _LCD_DISPLAY_OFF    0x00
+#define _LCD_CURSOR_ON      0x02
+#define _LCD_CURSOR_OFF     0x00
+#define _LCD_BLINK_ON       0x01
+#define _LCD_BLINK_OFF      0x00
+
+/************************************************************************/
+/* ENTRAMOS EN CURSOR OR DISPLAY SHIFT:  D7 D6 D5 D4  D3 D2  D1 D0      */
+/*      						         0  0  0  1  S/C R/L  *  *      */
+/*----------------------------------------------------------------------*/
+/*      S/C = 1: display shift                                          */
+/*		      0: cursor move                                            */
+/*		R/L = 1: shift to the right                                     */
+/*		      0: shift to the left                                      */
+/************************************************************************/
+#define _LCD_CURSORDISPLAYSHIFT 0x10
+#define _LCD_DISPLAY_SHIFT      0x08
+#define _LCD_CURSOR_SHIFT       0x00
+#define _LCD_MOVERIGHT          0x04
+#define _LCD_MOVELEFT           0x00
+
+/************************************************************************/
+/*      ENTRAMOS EN FUNTIONS SET:  D7 D6 D5 D4  D3 D2 D1 D0             */
+/*      						   0  0  1  D/L  N  F  *  *             */
+/*----------------------------------------------------------------------*/
+/*      D/L = 1: modo 8 bits                                            */
+/*		      0: modo 4 btis                                            */
+/*		N   = 1: MODO 2 Lineas                                          */
+/*		      0: MODO 1 Linea                                           */
+/*		F   = 1: MATRIZ 5x10                                            */
+/*		      0: MATRIZ 5x7/5x8                                         */
+/************************************************************************/
+#define _LCD_FUNTIONSET 0x20
+#define _LCD_8BITMODE   0x10
+#define _LCD_4BITMODE   0x00
+#define _LCD_2LINE      0x08
+#define _LCD_1LINE      0x00
+#define _LCD_5x10DOTS   0x04
+#define _LCD_5x8DOTS    0x00
+
+
+/************************************************************************/
+/*      SET CGRAM:  D7 D6  D5  D4   D3   D2   D1   D0                   */
+/*      			0  1   ACG ACG  ACG  ACG  ACG  ACG                  */
+/*----------------------------------------------------------------------*/
+/*      ACG -> CGRAM ADDRESS                                            */
+/************************************************************************/
+#define _LCD_SET_CGRAM_ADDR  0x40
+
+/************************************************************************/
+/*      SET DDRAM:  D7 D6  D5  D4   D3   D2   D1   D0                   */
+/*      			1  0   ADD ADD  ADD  ADD  ADD  ADD                  */
+/*----------------------------------------------------------------------*/
+/*      ADD -> DDRAM ADDRESS                                            */
+/************************************************************************/
+#define _LCD_SET_DDRAM_ADDR  0x80
+
+/************************************************************************/
+/* METODOS DE LIBRERIA                                                  */
+/************************************************************************/
+
+void lcd_init(void);
+void lcd_write(uint8_t letra);
+void lcd_command(uint8_t cmd);
+
+void lcd_puts(char *str);
+void lcd_set_cursor(uint8_t row, uint8_t col);
+void lcd_printf(char *str, ...);
+
+void lcd_clear(void);
+void lcd_return_home(void);
+void lcd_on(void);
+void lcd_off(void);
+
+void lcd_enable_blink(void);
+void lcd_disable_blink(void);
+
+void lcd_enable_cursor(void);
+void lcd_disable_cursor(void);
+
+void lcd_scroll_left(void);
+void lcd_scroll_right(void);
+
+void lcd_custom_char(uint8_t mem, uint8_t *charmap);
+
+void lcd_out(char row, char col, char *str);
+void lcd_char(char row, char col, char letra);
+
+#endif	
 
